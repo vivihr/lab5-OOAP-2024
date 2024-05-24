b3a341b829c4b8a7d7a1715c2780308d7a5a91ea
admin <admin@DESKTOP-M40D7GV>
2024-05-24 11:41:12 +0300

    Add project files.

---
+++ lab5NEW/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace PictureEditing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.clarityButton = new System.Windows.Forms.Button();
+            this.noiseButton = new System.Windows.Forms.Button();
+            this.saturationButton = new System.Windows.Forms.Button();
+            this.blurButton = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clarityButton
+            // 
+            this.clarityButton.Location = new System.Drawing.Point(436, 210);
+            this.clarityButton.Name = "clarityButton";
+            this.clarityButton.Size = new System.Drawing.Size(130, 56);
+            this.clarityButton.TabIndex = 20;
+            this.clarityButton.Text = "clarity";
+            this.clarityButton.UseVisualStyleBackColor = true;
+            this.clarityButton.Click += new System.EventHandler(this.clarityButton_Click);
+            // 
+            // noiseButton
+            // 
+            this.noiseButton.Location = new System.Drawing.Point(762, 210);
+            this.noiseButton.Name = "noiseButton";
+            this.noiseButton.Size = new System.Drawing.Size(130, 56);
+            this.noiseButton.TabIndex = 19;
+            this.noiseButton.Text = "artistic noise";
+            this.noiseButton.UseVisualStyleBackColor = true;
+            this.noiseButton.Click += new System.EventHandler(this.noiseButton_Click);
+            // 
+            // saturationButton
+            // 
+            this.saturationButton.Location = new System.Drawing.Point(595, 210);
+            this.saturationButton.Name = "saturationButton";
+            this.saturationButton.Size = new System.Drawing.Size(130, 56);
+            this.saturationButton.TabIndex = 18;
+            this.saturationButton.Text = "saturation";
+            this.saturationButton.UseVisualStyleBackColor = true;
+            this.saturationButton.Click += new System.EventHandler(this.saturationButton_Click);
+            // 
+            // blurButton
+            // 
+            this.blurButton.Location = new System.Drawing.Point(274, 210);
+            this.blurButton.Name = "blurButton";
+            this.blurButton.Size = new System.Drawing.Size(130, 56);
+            this.blurButton.TabIndex = 17;
+            this.blurButton.Text = "Blur";
+            this.blurButton.UseVisualStyleBackColor = true;
+            this.blurButton.Click += new System.EventHandler(this.blurButton_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(869, 316);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(240, 298);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(67, 302);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(232, 312);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(408, 293);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(338, 340);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(359, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(429, 148);
+            this.listBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 705);
+            this.Controls.Add(this.clarityButton);
+            this.Controls.Add(this.noiseButton);
+            this.Controls.Add(this.saturationButton);
+            this.Controls.Add(this.blurButton);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clarityButton;
+        private System.Windows.Forms.Button noiseButton;
+        private System.Windows.Forms.Button saturationButton;
+        private System.Windows.Forms.Button blurButton;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ lab5NEW/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+
+namespace PictureEditing
+{
+
+
+    public partial class Form1 : Form
+    {
+        private List<PictureBox> pictureBoxes;
+        private ImageFacade imageFacade;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePictureBoxes();
+            imageFacade = new ImageFacade(pictureBoxes);
+        }
+
+        private void InitializePictureBoxes()
+        {
+            pictureBoxes = new List<PictureBox> { pictureBox1, pictureBox2, pictureBox3 };
+        }
+
+        private void UpdateListBox()
+        {
+            listBox1.Items.Clear();
+            foreach (var pictureBox in pictureBoxes)
+            {
+                listBox1.Items.Add($"Name: {pictureBox.Name}, Size: {pictureBox.Image.Size}");
+            }
+        }
+
+        private void ApplyFilter(IFilter filter)
+        {
+            foreach (var pictureBox in pictureBoxes)
+            {
+                pictureBox.Image = filter.Apply(pictureBox.Image);
+            }
+            UpdateListBox();
+        }
+
+        private void blurButton_Click(object sender, EventArgs e)
+        {
+            ApplyFilter(new BlurFilter());
+        }
+
+        private void clarityButton_Click(object sender, EventArgs e)
+        {
+            ApplyFilter(new ClarityFilter());
+        }
+
+        private void saturationButton_Click(object sender, EventArgs e)
+        {
+            ApplyFilter(new SaturationFilter());
+        }
+
+        private void noiseButton_Click(object sender, EventArgs e)
+        {
+            ApplyFilter(new NoiseFilter());
+        }
+        
+        //фільтри
+        interface IFilter
+        {
+            Image Apply(Image image);
+        }
+
+        //клас для керування рисунками
+        class ImageFacade
+        {
+            private readonly List<PictureBox> pictureBoxes;
+
+            public ImageFacade(List<PictureBox> pictureBoxes)
+            {
+                this.pictureBoxes = pictureBoxes;
+            }
+
+            public void ApplyFilter(IFilter filter)
+            {
+                foreach (var pictureBox in pictureBoxes)
+                {
+                    pictureBox.Image = filter.Apply(pictureBox.Image);
+                }
+            }
+        }
+
+        //фільтри
+        class BlurFilter : IFilter
+        {
+            public Image Apply(Image image)
+            {
+                //розмиття
+                return ApplyBlurFilter(image);
+            }
+
+            private Image ApplyBlurFilter(Image image)
+            {
+                Bitmap bitmap = new Bitmap(image);
+                //логіка розмиття
+                AForge.Imaging.Filters.Blur blurFilter = new AForge.Imaging.Filters.Blur();
+                return blurFilter.Apply(bitmap);
+            }
+        }
+
+
+        class ClarityFilter : IFilter
+        {
+            public Image Apply(Image image)
+            {
+                //підвищення чіткості
+                return ApplyClarityFilter(image);
+            }
+
+            private Image ApplyClarityFilter(Image image)
+            {
+                Bitmap bitmap = new Bitmap(image);
+
+                //AForge.NET для підвищення чіткості
+                AForge.Imaging.Filters.Sharpen clarityFilter = new AForge.Imaging.Filters.Sharpen();
+                return clarityFilter.Apply(bitmap);
+            }
+        }
+
+        class SaturationFilter : IFilter
+        {
+            public Image Apply(Image image)
+            {
+                return ApplySaturationFilter(image);
+            }
+
+            private Image ApplySaturationFilter(Image image)
+            {
+                Bitmap bitmap = new Bitmap(image);
+                AForge.Imaging.Filters.SaturationCorrection saturationFilter = new AForge.Imaging.Filters.SaturationCorrection();
+
+                return saturationFilter.Apply(bitmap);
+            }
+        }
+
+        class NoiseFilter : IFilter
+        {
+            public Image Apply(Image image)
+            {
+                //приклад простого додавання шуму
+                return ApplyNoiseFilter(image);
+            }
+
+            private Image ApplyNoiseFilter(Image image)
+            {
+                Bitmap bitmap = new Bitmap(image);
+
+                //переведення зображення до підтримуваного формату
+                if (bitmap.PixelFormat != System.Drawing.Imaging.PixelFormat.Format24bppRgb)
+                {
+                    bitmap = AForge.Imaging.Image.Clone(bitmap, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                }
+
+                //створення фільтру шуму
+                AForge.Imaging.Filters.AdditiveNoise noiseFilter = new AForge.Imaging.Filters.AdditiveNoise();
+
+                //застосування фільтру до зображення
+                return noiseFilter.Apply(bitmap);
+            }
+        }
+
+    }
+}
